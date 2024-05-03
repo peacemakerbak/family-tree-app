b939988a785f63a78f4d54bca5684854b6c900a8
peacemakerbak <peacemakerbak@gmail.com>
2024-05-02 22:47:56 -0400

    home page done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,23 @@
 // This is the landing page component of the Family Tree App.
 // It includes a signup form for new users and a link to the login page for existing users.
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './HomePage.css';
-import Footer from './Footer';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+import Footer from "./Footer";
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <div className="welcome-card">
-        <h1>Welcome to the Family Tree Project</h1>
-        <p>Build your family tree and connect with your lineage.</p>
+        <h1>Build Your Family Tree</h1>
+        <p>
+          Welcome to my Family Tree Project! Here, you can collaborate with
+          your family members to construct a comprehensive family tree. Simply
+          provide us with your direct line of ancestry, and our platform will
+          seamlessly integrate your contributions into a beautifully organized
+          family tree.
+        </p>
       </div>
       <div className="signup-container">
         <h2 className="signup-title">Create Account</h2>
@@ -26,9 +32,6 @@ const HomePage = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default HomePage;
-
-
-
